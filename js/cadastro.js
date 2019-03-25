04e6f9af218bec890fa814b70ddb7711ca4874bb
FRANCIELE VELA DE ARAUJO <franciele.varaujo@senacsp.edu.br>
2019-03-25 17:43:11 -0300

    meu primeiro projeto

---
+++ js/cadastro.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    const _url='http://localhost:3000/alunos';
+    $("#cadastrar").on("click", function(){
+        var nome = $("#nome").val();
+        var sexo = $("#sexo option:selected").val();
+        // console.log(nome,sexo);
+
+        $.ajax({
+            type:"POST",
+            url:`${_url}`,
+            dataType:"json",
+            data:{nome: nome, sexo: sexo},
+            success:function(data){
+                if(data.id != null){
+                    console.log('Cadastrou');
+
+                }
+
+            },
+            error:function(data){
+
+            }
+        })
+
+
+    })
+})
